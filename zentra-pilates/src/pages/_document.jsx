21da4ae86ba0b1dfd9933dc80fb7a24f17bf926f
Nicolás Heredia <nicolasheredia02@gmail.com>
2023-06-16 21:47:34 -0300

    Pagina terminada

--- zentra-pilates/src/pages/_document.jsx
+++ zentra-pilates/src/pages/_document.jsx
@@ -5,11 +5,16 @@ export default function Document() {
     <Html lang="es">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&family=Roboto:wght@100;400;500;700&display=swap"
           rel="stylesheet"
         />
+        {/* <link rel="shortcut icon" href="../public/miniLogo.ico"/> */}
       </Head>
       <body>
         <Main />
