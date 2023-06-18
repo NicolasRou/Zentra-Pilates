65b91670c9829f23315613d05f69bec4d87aa189
Nicolás Heredia <nicolasheredia02@gmail.com>
2023-06-18 01:12:42 -0300

    Frontend terminado

--- zentra-pilates/next.config.js
+++ zentra-pilates/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap() {
+    return {
+      "/": { page: "/" },
+      "/horarios": { page: "/horarios" },
+      "/pagos": { page: "/pagos" },
+      "/planes": { page: "/planes" },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
