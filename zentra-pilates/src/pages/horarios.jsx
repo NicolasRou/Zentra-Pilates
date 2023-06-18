c317ad87f02d8c4127db5a4de3959a6ece75d417
Nicolás Heredia <nicolasheredia02@gmail.com>
2023-06-18 01:12:42 -0300

    Frontend terminado

--- zentra-pilates/src/pages/horarios.jsx
+++ zentra-pilates/src/pages/horarios.jsx
@@ -16,54 +16,55 @@ export default function Horarios() {
         content="Consultá por nuestros horarios. Zentra Estudio de Pilates, Yoga y stretching. En magallanes 1256, Cordón, Montevideo"
       >
         <div>
-          <div>
+          <div className={styles.container__portada}>
             <Image
               src={portada}
               className={styles.bg__img}
               alt="Imagen de portada pilates"
             />
-          </div>
-          <div className={styles.portada}>
-            <h3>Horarios de actividades</h3>
-            <div className={styles.container__table}>
-              <div className={styles.table__item}>
-                <Image
-                  src={reloj}
-                  width={30}
-                  className={styles.img}
-                  alt="icono reloj"
-                />
-                <p>HORARIOS</p>
-                <p>
-                  De lunes a <br /> sábados
-                </p>
+            <div className={styles.portada}>
+              <h3>Horarios de actividades</h3>
+              <div className={styles.container__table}>
+                <div className={styles.table__item}>
+                  <Image
+                    src={reloj}
+                    width={30}
+                    className={styles.img}
+                    alt="icono reloj"
+                  />
+                  <p>HORARIOS</p>
+                  <p>
+                    De lunes a <br /> sábados
+                  </p>
+                </div>
+
+                <div>
+                  <Image
+                    src={phone2}
+                    width={30}
+                    className={styles.img}
+                    alt="icono telefono"
+                  />
+                  <p>TELÉFONO</p>
+                  <p>098353971</p>
+                </div>
+                <div>
+                  <Image
+                    src={pin}
+                    width={30}
+                    className={styles.img}
+                    alt="icono ubicación"
+                  />
+                  <p>DIRECCIÓN</p>
+                  <p>
+                    Magallanes 1256 <br /> Esq. Constituyente
+                  </p>
+                </div>
               </div>
-              <div>
-                <Image
-                  src={phone2}
-                  width={30}
-                  className={styles.img}
-                  alt="icono telefono"
-                />
-                <p>TELÉFONO</p>
-                <p>098353971</p>
+              <div className={styles.subtitle}>
+                <p>Presenciales: Grupos reducidos</p>
+                <p>Online: Clases por zoom</p>
               </div>
-              <div>
-                <Image
-                  src={pin}
-                  width={30}
-                  className={styles.img}
-                  alt="icono ubicación"
-                />
-                <p>DIRECCIÓN</p>
-                <p>
-                  Magallanes 1256 <br /> Esq. Constituyente
-                </p>
-              </div>
-            </div>
-            <div>
-              <p>Presenciales: Grupos reducidos</p>
-              <p>Online: Clases por zoom</p>
             </div>
           </div>
           <div className={styles.text}>
