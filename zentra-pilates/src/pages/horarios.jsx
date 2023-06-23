bf138e46a459b6ca6b3b97f52dded59a6c267c8b
Nicolás Heredia <nicolasheredia02@gmail.com>
2023-06-23 03:12:04 -0300

    feat (horarios) : Agregado de tabla de horarios fix (responisive): modificacion portada de responsive movil

--- zentra-pilates/src/pages/horarios.jsx
+++ zentra-pilates/src/pages/horarios.jsx
@@ -61,17 +61,166 @@ export default function Horarios() {
                   </p>
                 </div>
               </div>
-              <div className={styles.subtitle}>
+              <div className={styles.p}>
                 <p>Presenciales: Grupos reducidos</p>
                 <p>Online: Clases por zoom</p>
               </div>
             </div>
           </div>
+          <div className={styles.subtitle}>
+            <h3>¡Elegí tu horario y contactanos!</h3>
+          </div>
           <div className={styles.text}>
-            <h3>Horarios de actividades</h3>
-            <p>
-              Consultá horarios con cupos disponibles por Whatsapp al 098353971
-            </p>
+            <table className={styles.table__container}>
+              <thead>
+                <tr className={styles.días}>
+                  <th className={styles.icon}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="icon icon-tabler icon-tabler-clock-hour-3"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                      <path d="M12 12h3.5"></path>
+                      <path d="M12 7v5"></path>
+                    </svg>
+                  </th>
+                  <th>Lunes</th>
+                  <th>Martes</th>
+                  <th>Miércoles</th>
+                  <th>Jueves</th>
+                  <th>Viernes</th>
+                  <th>Sábado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className={styles.hora}>09:00</td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>-</td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>11:00</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>12:00</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>
+                    <p> Pilates Stretching</p>
+                  </td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>17:00</td>
+                  <td>
+                    <p>TRX presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial + Stretching pres.</p>
+                  </td>
+                  <td>-</td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>18:30</td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial + TRX pres.</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>-</td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>19:45</td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Funcional + Stretching pres.</p>
+                  </td>
+                  <td>
+                    <p>TRX + Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>TRX + Pilates presencial</p>
+                  </td>
+                  <td>-</td>
+                </tr>
+                <tr>
+                  <td className={styles.hora}>20:45</td>
+                  <td>
+                    <p>Pilates funcional presencial</p>
+                  </td>
+                  <td>
+                    <p>Stretching presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>Pilates presencial</p>
+                  </td>
+                  <td>
+                    <p>TRX + Pilates presencial</p>
+                  </td>
+                  <td>-</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
           <Actividades />
           <Reserva />
