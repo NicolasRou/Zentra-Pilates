1e07c2804baf941786d2ad8c30e519d5a13bd08b
Nicolas <nicolasheredia02@gmail.com>
2023-10-29 01:22:53 -0300

    fix: app

--- zentra-backend/app.js
+++ zentra-backend/app.js
@@ -4,17 +4,10 @@ const horariosRoutes = require("./routes/horarios");
 const adminRoutes = require("./routes/admin")
 const app = express();
 const cors = require("cors");
-const i18n = require('i18n');
+
 
 const PORT = process.env.PORT || 5000;
 
-i18n.configure({
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
-    directory: __dirname + '/locales',
-});
-
-app.use(i18n.init);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
